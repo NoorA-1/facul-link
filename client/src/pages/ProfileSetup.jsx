7f82831667e3e305813a18213e3a4344d4e1764e
NoorA-1 <noorahmedd2288@gmail.com>
2024-08-13 13:06:57 +0500

    cookie fix

--- client/src/pages/ProfileSetup.jsx
+++ client/src/pages/ProfileSetup.jsx
@@ -15,7 +15,7 @@ export const loader = async () => {
     const { data } = await http.get("/users/current-user");
     return data;
   } catch (error) {
-    // console.log(error);
+    console.log(error);
     // return redirect("/");
 
     return null;
