318951c5b3e9f06f32285a8c9d1cf50d809c71b0
NoorA-1 <noorahmedd2288@gmail.com>
2024-08-13 10:34:10 +0500

    vite config fix

--- client/src/utils/http.js
+++ client/src/utils/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKENDURL}/api/v1`,
+  baseURL: `/api/v1`,
 });
 
 export default http;
--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: `${import.meta.env.VITE_BACKENDURL}/api`,
+        target: `https://facul-link-api.vercel.app/api`,
         changeOrigin: true,
         secure: false, //extra added
         rewrite: (path) => path.replace(/^\/api/, ""),
