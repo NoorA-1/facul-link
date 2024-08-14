f5e3b7c33c38a79efa2ec7fb663a4b70d5919227
NoorA-1 <noorahmedd2288@gmail.com>
2024-08-14 20:24:10 +0500

    Added cors config to socket io

--- server/app.js
+++ server/app.js
@@ -14,6 +14,9 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.CORS_ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   },
 });
 
